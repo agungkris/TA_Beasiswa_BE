d6dd244959f14fba8ea3ba53c0514e764f10e611
antonius joshua <lietschie@outlook.com>
2020-07-10 18:46:31 +0700

    period

---
+++ app/Period.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Period extends Model
+{
+    protected $table = 'periods';
+
+    protected $fillable = [
+        'period_name',
+        'description'
+    ];
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::prefix('period')->group(function(){
+    Route::get('/','PeriodController@index');
+    Route::post('/','PeriodController@store');
+    Route::get('/{id}','PeriodController@show');
+    Route::post('/{id}','PeriodController@update');
+    Route::delete('/{id}','PeriodController@destroy');
+});
