d3658b3c6c9027a8d48756f2bdd7344375d333cf
antonius joshua <lietschie@outlook.com>
2020-08-03 13:14:38 +0700

    add laravel module

---
+++ Modules/CommandCenter/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('commandcenter::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('commandcenter.name') !!}
+    </p>
+@endsection
