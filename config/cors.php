5fbb183111ffeb8f77ce6f509aceeb677fbe4eb8
agungkris <72875327+agungkris@users.noreply.github.com>
2021-05-06 01:25:35 +0700

    update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['POST', 'GET', 'DELETE', 'PUT', 'OPTIONS', 'PATCH', 'PUT', '*'],
 
-    'allowed_origins' => ['http://localhost:8080', 'https://beasiswaupj.herokuapp.com'],
+    'allowed_origins' => ['http://localhost:8080', 'http://upjtesting.xyz'],
     // 'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
